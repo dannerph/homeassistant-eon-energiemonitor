6c6d569262169092122b066876220c31f4ab64e7
Philipp Danner <philipp@danner-web.de>
2021-12-23 17:08:05 +0100

    Cleanup

--- custom_components/eon-energiemonitor/sensor.py
+++ custom_components/eon-energiemonitor/sensor.py
@@ -1,11 +1,12 @@
 """Support for EON Energiemonitor."""
 import logging
 
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
 
 from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the EON Energiemonitor sensors."""
@@ -31,27 +32,20 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     async_add_entities(sensors)
 
 
-class EONEnergySensor(Entity):
+class EONEnergySensor(SensorEntity):
     """The entity class for EON Energiemonitor charging stations sensors."""
+
+    _attr_should_poll = False
 
     def __init__(self, name, eon_energiemontior):
         """Initialize the EON Energiemonitor Sensor."""
         self._name = name
         self._eon_energiemonitor = eon_energiemontior
         self._icon = "mdi:flash"
-        self._state = None
         self._attributes = {}
-        self._unit_of_measurement = None
 
-    @property
-    def unique_id(self):
-        """Return the unique ID of the binary sensor."""
-        return f"eon_energy_{self._name}"
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return f"eon_energiemonitor_{self._name.capitalize()}"
+        self._attr_name = f"eon_energiemonitor_{self._name.capitalize()}"
+        self._attr_unique_id = f"eon_energy_{self._name}"
 
     @property
     def icon(self):
@@ -59,37 +53,19 @@ class EONEnergySensor(Entity):
         return self._icon
 
     @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Get the unit of measurement."""
-        return self._unit_of_measurement
-
-    @property
-    def should_poll(self) -> bool:
-        """Return True if entity has to be polled for state.
-
-        False if entity pushes its state to HA.
-        """
-        return False
-
-    @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes of the binary sensor."""
         return self._attributes
 
     async def async_update(self):
         """Get latest cached states from the device."""
-        
+
         data = self._eon_energiemonitor.get_data(self._name)
         _LOGGER.debug(data)
         if data is not None:
-            self._state = round(float(data.get('state')), 1)
-            self._attributes = data.get('attributes')
-            self._unit_of_measurement = data.get('unit')
+            self._attr_native_value = round(float(data.get("state")), 1)
+            self._attributes = data.get("attributes")
+            self._attr_native_unit_of_measurement = data.get("unit")
 
     def update_callback(self):
         """Schedule a state update."""
